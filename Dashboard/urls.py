28cb96aa963b563711cc7e0284722d4c5da43e5d
Ammar sahyoun <ammarzgr@hotmail.com>
2020-05-25 23:28:18 +0200

    Rearrange app files and config

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Dashboard.views import home
+
+from .views import home
 
 urlpatterns = [
     path('', home, name="home"),
